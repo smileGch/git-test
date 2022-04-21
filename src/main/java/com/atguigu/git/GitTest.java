379f89b82ee05df99db06e69fc6ffa56f38021b6
13048 <1304891265@qq.com>
2022-04-21 10:42:08 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author smilegc
+ * @create 2022-04-21 10:39
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
